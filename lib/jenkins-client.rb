81bef3809bee8fe420265d1fa68ccc806ef1d7a5
John Griffin <johnog@gmail.com>
2012-02-07 10:22:50 +0000

    initial import

---
+++ lib/jenkins-client.rb
@@ -0,0 +1,7 @@
+require "jenkins-client/version"
+
+module Jenkins
+  module Client
+    # Your code goes here...
+  end
+end
---
+++ lib/jenkins-client/version.rb
@@ -0,0 +1,5 @@
+module Jenkins
+  module Client
+    VERSION = "0.0.1"
+  end
+end
