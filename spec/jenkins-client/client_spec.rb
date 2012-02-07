b54745d8b3a3f5adb9f5c18eff3b36b9c4d5fc2d
John Griffin <johnog@gmail.com>
2012-02-07 17:05:28 +0000

    update test fixture

--- spec/jenkins-client/client_spec.rb
+++ spec/jenkins-client/client_spec.rb
@@ -13,10 +13,10 @@ describe Jenkins::Client do
     it "will return a list of jobs" do
       body = '{"assignedLabels":[{}],"mode":"NORMAL","nodeDescription":"the master Jenkins node","nodeName":"","numExecutors":2,"description":null,"jobs":[
       {"name":"foo-bar","url":"https://testjenkins.com/job/foo-bar/","color":"blue"},
-      {"name":"baton-deploy","url":"https://jenkins.dsci.it/job/baton-deploy/","color":"red"},
-      {"name":"baton-ping","url":"https://jenkins.dsci.it/job/baton-ping/","color":"red"},
-      {"name":"central-apu","url":"https://jenkins.dsci.it/job/central-apu/","color":"blue"},
-      {"name":"central-camac","url":"https://jenkins.dsci.it/job/central-camac/","color":"blue"}]}'
+      {"name":"woohoo","url":"https://jenkinstest.com/job/woohoo/","color":"red"},
+      {"name":"wat","url":"https://jenkinstest.com/job/wat/","color":"red"},
+      {"name":"fudge","url":"https://jenkinstest.com/job/fudge/","color":"blue"},
+      {"name":"amaze","url":"https://jenkinstest.com/job/amaze/","color":"blue"}]}'
       stub_request(:get, "https://testuser:testpass@jenkinstest.com/api/json").
          with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
          to_return(:status => 200, :body => body, :headers => {})
