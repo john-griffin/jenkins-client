23fd489a998d767a687d2981e47d1ad5ebc05dca
John Griffin <johnog@gmail.com>
2012-02-08 09:30:59 +0000

    refactor specs

--- spec/jenkins-client/job_spec.rb
+++ spec/jenkins-client/job_spec.rb
@@ -12,20 +12,32 @@ describe Jenkins::Client::Job do
   end
 
   describe ".all" do
-    it "will return a list of jobs" do
-      body = '{"assignedLabels":[{}],"mode":"NORMAL","nodeDescription":"the master Jenkins node","nodeName":"","numExecutors":2,"description":null,"jobs":[
-      {"name":"foo-bar","url":"https://testjenkins.com/job/foo-bar/","color":"blue"},
-      {"name":"woohoo","url":"https://jenkinstest.com/job/woohoo/","color":"red"},
-      {"name":"wat","url":"https://jenkinstest.com/job/wat/","color":"red"},
-      {"name":"fudge","url":"https://jenkinstest.com/job/fudge/","color":"blue"},
-      {"name":"amaze","url":"https://jenkinstest.com/job/amaze/","color":"blue"}]}'
-      stub_request(:get, "https://testuser:testpass@jenkinstest.com/api/json").
-         with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
-         to_return(:status => 200, :body => body, :headers => {})
-      Jenkins::Client::Job.all.should include({"name"=>"foo-bar",
-        "url"=>"https://testjenkins.com/job/foo-bar/",
-        "color"=>"blue"})
-      Jenkins::Client::Job.all.should have(5).items
+    context "given some jobs are available" do
+      before(:each) do
+        body = '{"assignedLabels":[{}],"mode":"NORMAL","nodeDescription":"the master Jenkins node","nodeName":"","numExecutors":2,"description":null,"jobs":[
+        {"name":"foo-bar","url":"https://testjenkins.com/job/foo-bar/","color":"blue"},
+        {"name":"woohoo","url":"https://jenkinstest.com/job/woohoo/","color":"red"},
+        {"name":"wat","url":"https://jenkinstest.com/job/wat/","color":"red"},
+        {"name":"fudge","url":"https://jenkinstest.com/job/fudge/","color":"blue"},
+        {"name":"amaze","url":"https://jenkinstest.com/job/amaze/","color":"blue"}]}'
+        stub_request(:get, "https://testuser:testpass@jenkinstest.com/api/json").
+           with(:headers => {'Accept'=>'*/*', 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3', 'User-Agent'=>'Ruby'}).
+           to_return(:status => 200, :body => body, :headers => {})        
+      end
+
+      let(:jobs){Jenkins::Client::Job.all}
+
+      it "will return a list of jobs" do
+        jobs.should have(5).items
+      end
+
+      it "will contain an expected job" do
+        jobs.should include({
+          "name"=>"foo-bar", 
+          "url"=>"https://testjenkins.com/job/foo-bar/",
+          "color"=>"blue"
+        })
+      end
     end
   end
 end
