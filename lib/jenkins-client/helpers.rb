0a5e123c5cc84ee9951f31763e843452f084061e
dblock <dblock@dblock.org>
2012-05-18 14:57:17 -0400

    Added netrc stuff.

--- lib/jenkins-client/helpers.rb
+++ lib/jenkins-client/helpers.rb
@@ -16,7 +16,31 @@ module Jenkins
       def longest(items)
         items.map { |i| i.to_s.length }.sort.last
       end
+
+      def home_directory
+        running_on_windows? ? ENV['USERPROFILE'].gsub("\\","/") : ENV['HOME']
+      end
+
+      def running_on_windows?
+        RUBY_PLATFORM =~ /mswin32|mingw32/
+      end
+
+      def running_on_a_mac?
+        RUBY_PLATFORM =~ /-darwin\d/
+      end
+
+      def ask
+        STDIN.gets.strip
+      end
       
+      def with_tty(&block)
+        return unless $stdin.tty?
+        begin
+          yield
+        rescue
+          # fails on windows
+        end
+      end
     end
   end
 end
